e1cfe6d228a543ba1a4d4e9eb1ff0c92efc9c0f5
Sergio Lopez Perez <l17161158@tecnm.oaxaca.mx>
2024-06-02 22:52:14 -0600

    integracion d ela vistas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { RechargesComponent } from './recharges/recharges.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    HomeComponent,
+    RechargesComponent,
+    ServicesComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'prueba_gestopago';
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'; // Ejemplo de componente
+import { RechargesComponent } from './recharges/recharges.component'; // Ejemplo de componente
+import { ServicesComponent } from './services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: HomeComponent, title: 'Inicio'
+    }, // Ruta principal
+    {
+        path: 'recharges', component: RechargesComponent, title: 'Recargas'
+    },
+    {
+        path: 'services', component: ServicesComponent, title: 'Servicios'
+    },
+];
